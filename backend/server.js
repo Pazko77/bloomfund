2961a4bb5b53058abf32438363527eb985a45f48
Ychnightder <pierreychnightder561@gmail.com>
2025-12-15 00:21:50 +0100

    ajoute des config backend node js

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const pool = require('./config/db');
+
+const app = express();
+app.use(express.json());
+
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
