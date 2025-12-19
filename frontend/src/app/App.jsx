68411701e65ceb3e823b2c74e066f4f44df9e05f
Ychnightder <pierreychnightder561@gmail.com>
2025-12-19 01:36:28 +0100

    modification pour respecter le design

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -7,7 +7,6 @@ import Inscription from '../pages/Inscription/Inscription.jsx';
 import Rechercher from '../pages/Rechercher/Rechercher.jsx';
 import Cagnotte from '../pages/FormulaireCagnotte/FormulaireCagnotte.jsx';
 import LayoutMain from '../layouts/LayoutMain/LayoutMain.jsx';
-import LayoutAuth from '../layouts/LayoutMain/LayoutAuth.jsx';
 function App() {
 	return (
 		<>
@@ -15,16 +14,12 @@ function App() {
 				{/* Pages AVEC navbar */}
 				<Route element={<LayoutMain />}>
 					<Route path="/" element={<Accueil />} />
-					<Route path="/Accueil" element={<Accueil />} />
+					<Route path="/accueil" element={<Accueil />} />
 					<Route path="" element={<Accueil />} />
 					<Route path="/about" element={<About />} />
-					<Route path="/rechercher" element={<Rechercher />} />
-				</Route>
-
-				{/* Pages SANS navbar */}
-				<Route element={<LayoutAuth />}>
 					<Route path="/connexion" element={<Connexion />} />
 					<Route path="/inscription" element={<Inscription />} />
+					<Route path="/rechercher" element={<Rechercher />} />
 					<Route path="/formCagnotte" element={<Cagnotte />} />
 				</Route>
 			</Routes>
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,32 +1,33 @@
 import './Navbar.scss';
 import logoNoText from '/BloomfundNoText.svg';
 import searchIcon from '/search.svg';
+import logoText from '/BloomfundLogo.svg';
 import { useLocation } from 'react-router-dom';
+
 function Navbar() {
-	const navLinks = [
-		{ href: '/rechercher', text: 'Rechercher', icon: searchIcon },
-		{ href: '/about', text: 'À propos' },
-		{ href: '/', icon: logoNoText, alt: 'LogoBloomFund' },
-		{ href: '/form', text: 'Démarrer une cagnotte' },
-		{ href: '/connexion', text: 'Connexion' },
-	];
+	const location = useLocation();
 
-    	const location = useLocation(); 
+	const formRoutes = ['/form', '/formCagnotte', '/inscription', '/connexion'];
+	const isFormPage = formRoutes.includes(location.pathname);
 
-        const hiddenRoutes = ['/connexion', '/inscription'];
-
-        if (hiddenRoutes.includes(location.pathname)) {
-            return null;
-        }
+	const navLinks = isFormPage
+		? [{ href: '/', icon: logoText, alt: 'LogoBloomFund' }]
+		: [
+				{ href: '/rechercher', text: 'Rechercher', icon: searchIcon },
+				{ href: '/about', text: 'À propos' },
+				{ href: '/', icon: logoNoText, alt: 'LogoBloomFund' },
+				{ href: '/form', text: 'Démarrer une cagnotte' },
+				{ href: '/connexion', text: 'Connexion' },
+			];
 
 	return (
-		<div className="navbar">
+		<div className={`navbar ${isFormPage ? 'navbar--logo-only' : ''}`}>
 			<nav>
 				<ul>
 					{navLinks.map((link, index) => (
 						<li key={index}>
 							<a className="navbar-item" href={link.href}>
-								{link.icon && <img src={link.icon} alt={link.alt || ''} />}
+								{link.icon && <img width={'162px'} src={link.icon} alt={link.alt || ''} />}
 								{link.text && ` ${link.text}`}
 							</a>
 						</li>
