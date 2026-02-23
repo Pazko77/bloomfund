5f212f9b5e38faba02edacd4e7a51c6c6c02a30b
Pascal Liu <pascal.liu@etu.u-paris.fr>
2026-02-23 20:54:52 +0100

    update multi-server.ts, vercel.json

--- backend/multi-servers.ts
+++ backend/multi-servers.ts
@@ -34,10 +34,12 @@ export default function createApp() {
 	return app;
 }
 
-const ports = process.env.PORTS ? process.env.PORTS.split(',').map(Number) : [3000, 3001];
-ports.forEach(port => {
-	const app = createApp();
-	app.listen(port, () => {
-		console.log(`-|Local: http://localhost:${port}/|-`);
+if (process.env.NODE_ENV !== 'production') {
+	const ports = process.env.PORTS ? process.env.PORTS.split(',').map(Number) : [3000, 3001];
+	ports.forEach(port => {
+		const localApp = createApp();
+		localApp.listen(port, () => {
+			console.log(`-|Local: http://localhost:${port}/|-`);
+		});
 	});
-});
+}
